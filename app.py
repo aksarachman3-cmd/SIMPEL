2b471d3faaec8a0c777b0c576551ff51fb907797
aksarachman3-cmd <aksarachman3@gmail.com>
2026-06-15 21:02:45 +0700

    Initial setup: folder structure, venv, hello Streamlit

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="SIMPEL", page_icon="🧭")
+st.title("🧭 SIMPEL – Cek Bantuan Sosial")
+st.write("Selamat datang! Aplikasi ini akan membantu Anda mengecek bantuan sosial yang mungkin Anda dapat.")
